85f61700b74c0710aa1223880952188c5e563ee1
isabellalcvicente <isabellavicente@ufpr.br>
2023-06-25 15:28:04 -0300

    criado as funcoes de conversao de comprimento

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ var origemTemp=document.getElementById('origemTemp');
 var destinoCompr=document.getElementById('destinoCompr');
 var destinoPeso=document.getElementById('destinoPeso');
 var destinoTemp=document.getElementById('destinoTemp');
+
+// funcoes de comprimento
+function metrosCentimetros(metros){
+    return metros*100;
+  }
+  function metrosPolegadas(metros){
+    return metros*39,37;
+  }
+  function centimetrosMetros(centimetros){
+    return centimetros/100;
+  }
+  function centimetrosPolegadas(centimetros){
+    return centimetros/2,54;
+  }
+  function polegadasMetros(polegadas){
+    return polegadas/39,37;
+  }
+  function polegadasCentimetros(polegadas){
+    return polegadas*2,54;
+  }
+  
