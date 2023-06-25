78b150090e4614bf6292a04bc51b71e655ab1303
isabellalcvicente <isabellavicente@ufpr.br>
2023-06-25 15:26:48 -0300

    selecionado as variaveis de entrada

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+var origemCompr=document.getElementById('origemCompr');
+var origemPeso=document.getElementById('origemPeso');
+var origemTemp=document.getElementById('origemTemp');
+var destinoCompr=document.getElementById('destinoCompr');
+var destinoPeso=document.getElementById('destinoPeso');
+var destinoTemp=document.getElementById('destinoTemp');
